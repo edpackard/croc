d20658573463b5d798611cc0f3f431daa22df7ff
edpackard <edpackard81@gmail.com>
2021-10-02 15:13:25 +0100

    add player and centred flexbox

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const levelMap = [
-    ["Water", "Water", "Water", "Water", "Water"],
+    ["Water", "Water", "Player", "Water", "Water"],
     ["Water", "Water", "Croc", "Water", "Water"],
     ["Croc", "Croc", "Water", "Croc", "Croc"],
     ["Croc", "Croc", "Water", "Croc", "Croc"],
@@ -12,16 +12,20 @@ document.addEventListener("DOMContentLoaded", () => {
     for (let row = 0; row < levelMap.length; row++) {
       for (let col = 0; col < levelMap[row].length; col++) {
         let tile = document.createElement("img");
-        if (levelMap[row][col] === "Water") {
-          tile.setAttribute("src", "./images/water.png");
-        }
         if (levelMap[row][col] === "Croc") {
           tile.setAttribute("src", "./images/croc.png");
+        } else if (levelMap[row][col] === "Player") {
+          tile.setAttribute("src", "./images/swimmer.png");
+        } else {
+          tile.setAttribute("src", "./images/water.png");
         }
         tile.setAttribute("row", row); // might not need this
         tile.setAttribute("col", col); // might not need this
         gameGrid.appendChild(tile);
       }
+      // let breaker = document.createElement("div");
+      // breaker.setAttribute("class", "break");
+      // gameGrid.appendChild(breaker);
     }
   }
 
